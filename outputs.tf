85813014ef0a6d829c9eb7479b379004292f179f
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-04-15 11:48:01 -0400

    Replace hardcoded bucket names with a variable
    Also:
    - Make remote states use the same workspace name as the current Terraform workspace.
    - Remove references to production/staging resources.
    - Sort remote state config keys alphabetically while we are here.

--- outputs.tf
+++ outputs.tf
@@ -3,22 +3,12 @@ output "read_terraform_state" {
   value       = module.read_terraform_state
 }
 
-output "vmimport_role_production" {
-  description = "The ARN for the vmimport service role in the Images (Production) account."
-  value       = aws_iam_role.vmimport_production.arn
+output "vmimport_role" {
+  description = "The ARN for the vmimport service role in the Images account."
+  value       = aws_iam_role.vmimport.arn
 }
 
-output "vmimport_role_staging" {
-  description = "The ARN for the vmimport service role in the Images (Staging) account."
-  value       = aws_iam_role.vmimport_staging.arn
-}
-
-output "vmimportexportaccess_role_production" {
-  description = "The IAM role that can be assumed to manage VM Import/Export tasks in the Images (Production) account."
-  value       = aws_iam_role.vmimportexportaccess_production
-}
-
-output "vmimportexportaccess_role_staging" {
-  description = "The IAM role that can be assumed to manage VM Import/Export tasks in the Images (Staging) account."
-  value       = aws_iam_role.vmimportexportaccess_staging
+output "vmimportexportaccess_role" {
+  description = "The IAM role that can be assumed to manage VM Import/Export tasks in the Images account."
+  value       = aws_iam_role.vmimportexportaccess
 }
--- remote_states.tf
+++ remote_states.tf
@@ -8,78 +8,58 @@ data "terraform_remote_state" "assessment_images" {
   backend = "s3"
 
   config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
+    bucket         = var.terraform_state_bucket
     dynamodb_table = "terraform-state-lock"
-    profile        = "cool-terraform-backend"
-    region         = "us-east-1"
+    encrypt        = true
     key            = "cool-images-assessment-images/terraform.tfstate"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
   }
 
-  # There is only one environment for this remote state.
-  workspace = "default"
+  workspace = terraform.workspace
 }
 
-data "terraform_remote_state" "images_production" {
+data "terraform_remote_state" "images" {
   backend = "s3"
 
   config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
+    bucket         = var.terraform_state_bucket
     dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    key            = "cool-accounts/images.tfstate"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
-    key            = "cool-accounts/images.tfstate"
   }
 
-  workspace = "production"
-}
-
-data "terraform_remote_state" "images_staging" {
-  backend = "s3"
-
-  config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
-    dynamodb_table = "terraform-state-lock"
-    profile        = "cool-terraform-backend"
-    region         = "us-east-1"
-    key            = "cool-accounts/images.tfstate"
-  }
-
-  workspace = "staging"
+  workspace = terraform.workspace
 }
 
 data "terraform_remote_state" "terraform" {
   backend = "s3"
 
   config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
+    bucket         = var.terraform_state_bucket
     dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    key            = "cool-accounts/terraform.tfstate"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
-    key            = "cool-accounts/terraform.tfstate"
   }
 
-  # There is only one environment for this account, so there is
-  # no need to match the current Terraform workspace.
-  workspace = "production"
+  workspace = terraform.workspace
 }
 
 data "terraform_remote_state" "users" {
   backend = "s3"
 
   config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
+    bucket         = var.terraform_state_bucket
     dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    key            = "cool-accounts/users.tfstate"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
-    key            = "cool-accounts/users.tfstate"
   }
 
-  # There is only one environment for this account, so there is
-  # no need to match the current Terraform workspace.
-  workspace = "production"
+  workspace = terraform.workspace
 }
